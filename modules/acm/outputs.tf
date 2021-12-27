fe13e13e5c571949e9a7cdea9df7484f60d86968
Hameedullah Khan <hameed@sudo.inc>
2021-12-28 01:45:10 +0500

    Remove acm module from cloudfront

--- modules/acm/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "arn" {
-  value = aws_acm_certificate.acm_cert_cloudfront.arn
-}
--- modules/acm/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "domain_name" {
-  type        = string
-  description = "Specify the domain to host on cloudfront"
-}
-variable "zone_name" {
-  type        = string
-  description = "Specify the domain to host on cloudfront"
-  default = ""
-}
